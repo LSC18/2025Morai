8ac0e93e828e5fa8f946dc9bd75395e92833fc66
chamssal <chamssal1212@gmail.com>
2025-08-08 15:50:54 +0900

    250808 동규 - sequence_manager 노드에서 각 미션 done상태 받는 subscriber랑 cb함수 만듦! - 해야할거?     - 제어쪽을 떼서 여기랑 붙이고 각 미션 노드들이랑 유기적으로 잘 묶기~!

--- src/sequence_manager/scripts/sequence_manager_node.py
+++ src/sequence_manager/scripts/sequence_manager_node.py
@@ -1,27 +1,41 @@
 #!/usr/bin/env python3
 import rospy
-from std_msgs.msg import Int32
+from std_msgs.msg import Int32, Bool
 from sequence_enum import SequenceState
 
 class SequenceManager:
     def __init__(self):
         rospy.init_node("sequence_manager")
+        rospy.loginfo("[SequenceManager] SequenceManager 초기화.")
 
-        self.sequence = SequenceState.SLAM  # 초기값: SLAM 대기 상태
+        self.sequence = SequenceState.SLAM  # 초기값: SLAM 상태
         self.seq_pub = rospy.Publisher("/sequence/state", Int32, queue_size=1)
-
-        # SLAM 완료 체크 (파라미터 기반)
-        self.slam_check_timer = rospy.Timer(rospy.Duration(1.0), self.check_slam_done)
+        
+        self.slam_done_sub = rospy.Subscriber("/sequence/slam_done", Bool, self.slam_done_CB)
+        self.static_done_sub = rospy.Subscriber("/sequence/static_done", Bool, self.static_done_CB)
+        self.dynamic_done_sub = rospy.Subscriber("/sequence/dynamic_done", Bool, self.dynamic_done_CB)
 
         self.rate = rospy.Rate(10)
         self.run()
 
-    def check_slam_done(self, event):
-        if rospy.has_param("/slam_done") and rospy.get_param("/slam_done") is True:
-            if self.sequence == SequenceState.SLAM:
-                rospy.loginfo("[SequenceManager] SLAM 완료 감지됨. 차선 추종으로 이동.")
-                self.sequence = SequenceState.LANE_FOLLOWING
+    def slam_done_CB(self, msg):
+        if msg.data == True:
+            self.sequence = SequenceState.LANE_FOLLOWING
+        elif msg.data == False:
+            self.sequence = SequenceState.SLAM
 
+    def static_done_CB(self, msg):
+        if msg.data == True:
+            self.sequence = SequenceState.LANE_FOLLOWING
+        elif msg.data == False:
+            self.sequence = SequenceState.STATIC_OBSTACLE
+            
+    def dynamic_done_CB(self, msg):
+        if msg.data == True:
+            self.sequence = SequenceState.LANE_FOLLOWING
+        elif msg.data == False:
+            self.sequence = SequenceState.DYNAMIC_OBSTACLE
+            
     def run(self):
         while not rospy.is_shutdown():
             self.seq_pub.publish(self.sequence.value)
@@ -50,8 +64,6 @@ class SequenceManager:
 
     def handle_lane_following(self):
         rospy.loginfo_throttle(2.0, "[SequenceManager] 차선 추종 중...")
-        # 예: 특정 조건 만족 시 다음 시퀀스로
-        # self.sequence = SequenceState.STATIC_OBSTACLE
 
     def handle_static_obstacle(self):
         rospy.loginfo_throttle(2.0, "[SequenceManager] 정적 장애물 회피 중...")
