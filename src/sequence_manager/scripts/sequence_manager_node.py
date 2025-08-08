292cb0952062fb7e1ea0b6935eb21895e1c00f1c
chamssal <chamssal1212@gmail.com>
2025-08-08 18:33:19 +0900

    250808 동규 - 시퀀스매니저 고치고 서브컴에서 잘 돌아가는거 확인까지함.

--- src/sequence_manager/scripts/sequence_manager_node.py
+++ src/sequence_manager/scripts/sequence_manager_node.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
+
+import os
+import sys
+
+# ─── scripts/ -> 상위 폴더(lane_follower 패키지 루트) 경로를 PYTHONPATH에 추가 ───
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir  = os.path.abspath(os.path.join(current_dir, '..'))
+if parent_dir not in sys.path:
+    sys.path.insert(0, parent_dir)
+    
 import rospy
 from std_msgs.msg import Float64, Int32, Bool
-from sequence_enum import SequenceState
+from sequence_manager.sequence_enum import SequenceState
 
 class SequenceManager:
     def __init__(self):
